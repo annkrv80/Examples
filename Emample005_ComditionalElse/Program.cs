f99f11ca7191b478a8681b8b2dc94cbb5a0d796f
Anna <annkrv80@yandex.ru>
2022-08-05 02:13:00 +0700

    Emample005

---
+++ Emample005_ComditionalElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="женя")
+{
+    Console.WriteLine ("Любимый,я так скучала");
+}
+else
+
+{
+     Console.Write ("Привет,");
+     Console.Write (username);
+}
